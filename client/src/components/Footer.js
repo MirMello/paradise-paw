104b940dcf2631d1d14e4ff8e747993c134c6021
MirMello <mirandaleeallen@gmail.com>
2022-11-16 15:04:09 -0700

    added a book appointment form

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,8 +8,10 @@ function Footer() {
                     Call us at <tele>(801) 589-8924</tele>
                 </p>
                 <p>
-                   Give my facebook page a like! {/* <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100054300710319%26sk&width=450&layout=standard&action=like&size=small&share=true&height=35&appId" width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"/> */}
+                   Give my <a className='facebook-page-link' href="https://www.facebook.com/profile.php?id=100054300710319">facebook page</a> a like!
                 </p>
+
+                {/* facebook link isn't working */}
 
             </div>
             <div className='footer-column-2'>
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,18 +1,31 @@
 
 
-function Homepage ({setcurrentPage}) {
+function Homepage({ setcurrentPage }) {
     return (
         <body className="homepage-body">
             <h1>Welcome to Paradise Paw</h1>
-            <article className="about-me">
-                <h2 id="about-me">About Me</h2>
-                <p>I love what I do, and I love my clients. This is my online portfolio to share my passion with others</p>
-            </article>
-            <div className="fb-reviews">
+            <div className="homepage-container">
+                <div className="homepage-column-1">
+                    <article className="about-me">
+                        <h2 id="about-me">About Me</h2>
+                        <p>I love what I do, and I love my clients. This is my online portfolio to share my passion with others</p>
+                    </article>
+                </div>
+                <div className="homepage-column-2">
+                    <h3>Book Appointment</h3>
+                    <form className="appointment-form">
+                        <label for="contact-name">Your Name</label>
+                        <input type="text" id="contact-name" placeholder="Your Name" />
 
+                        <label for="contact-message">Message</label>
+                        <textarea id="contact-message" placeholder="Message"></textarea>
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
         </body>
-    )  
+    )
 };
 
 export default Homepage;
